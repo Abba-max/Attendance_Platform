d879f5e3a5969d452ab263f03808adeb01033bda
Abba-Max <adjiabba10@gmail.com>
2026-05-18 23:30:48 +0200

    fix(migration): resolve case mismatch in StudentMigrationController package declaration

--- src/main/java/group3/en/stuattendance/Institutionmanager/controller/StudentMigrationController.java
+++ src/main/java/group3/en/stuattendance/Institutionmanager/controller/StudentMigrationController.java
@@ -1,4 +1,4 @@
-package group3.en.stuattendance.Institutionmanager.controller;
+package group3.en.stuattendance.Institutionmanager.Controller;
 
 import group3.en.stuattendance.Institutionmanager.DTO.*;
 import group3.en.stuattendance.Institutionmanager.Service.StudentMigrationService;
