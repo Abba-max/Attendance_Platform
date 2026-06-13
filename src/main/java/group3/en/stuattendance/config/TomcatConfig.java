0dc68b086ee8aa7abcd346a2168a743966b4a008
Abba-Max <adjiabba10@gmail.com>
2026-06-13 09:36:46 +0200

    Configure simultaneous HTTP/HTTPS and dynamic base URL

---
+++ src/main/java/group3/en/stuattendance/config/TomcatConfig.java
@@ -0,0 +1,26 @@
+package group3.en.stuattendance.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Value("${server.http.port:8080}")
+    private int httpPort;
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return factory -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(httpPort);
+            // Optionally, we could set a redirect port here to force HTTPS:
+            // connector.setRedirectPort(8443);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}
